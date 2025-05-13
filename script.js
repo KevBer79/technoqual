dae113c42a976f1154170ec52a80df8b82fb3e31
KevBer79 <kevinbernardeaugirault@gmail.com>
2025-05-13 15:58:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -81,5 +81,5 @@ function fermerMessageErreur() {
 
 // Démarrer le décompte quand la page est chargée
 window.onload = function () {
-    startCountdown(20); // Lance le compte à rebours de 20 minutes
+    startCountdown(15); // Lance le compte à rebours de 15 minutes
 };
